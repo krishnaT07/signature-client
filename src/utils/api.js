238955a395f87f963f4e23b30df66477ecadd837
krishnaT07 <160775556+krishnaT07@users.noreply.github.com>
2025-07-05 18:10:13 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
- baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: 'https://signature-server-5olu.onrender.com/api',
    withCredentials: true,
 });
 
